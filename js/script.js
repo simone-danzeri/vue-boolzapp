d56b177d142014512f3dc0fc57ef81406c3cac60
simone-danzeri <s.danzeri@icloud.com>
2024-04-03 18:22:13 +0200

    fix: fixed a misstype in constacts array (a message was shown as received instead of sent)

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ createApp({
             {
               date: "20/03/2020 16:35:00",
               message: "Mi piacerebbe ma devo andare a fare la spesa.",
-              status: "received",
+              status: "sent",
             },
           ],
         },
